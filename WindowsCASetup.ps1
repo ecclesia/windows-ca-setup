2296412bda29387de8e905b9c058329e6724b043
Dominik Mähl <dominik.maehl@ecclesia-gruppe.de>
2023-12-06 08:12:06 +0100

    Temurin-Installationen finden

--- WindowsCASetup.ps1
+++ WindowsCASetup.ps1
@@ -1,5 +1,5 @@
 function Main() {
-    Write-Output "Ecclesia IT Windows CA Setup 5 (27.02.2023)"
+    Write-Output "Ecclesia IT Windows CA Setup 6 (17.11.2023)"
     Write-Output "=========================================================="
     CreateCAFile
     ConfigureCurl
@@ -15,22 +15,22 @@ function CreateCAFile() {
     $AllProtocols = [System.Net.SecurityProtocolType]'Tls11,Tls12'
     [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
     
-    Remove-Item -Path $cafile -ErrorAction SilentlyContinue
+    Remove-Item -Path $caFile -ErrorAction SilentlyContinue
     
-    Invoke-WebRequest -Uri "https://curl.haxx.se/ca/cacert.pem" -OutFile $cafile
+    Invoke-WebRequest -Uri "https://curl.haxx.se/ca/cacert.pem" -OutFile $caFile
 
-    Write-Output "Ecclesia Holding GmbH AD Root CA" | Add-Content -Path $cafile
-    Write-Output "================================" | Add-Content -Path $cafile
-    Write-Output $root_ca | Add-Content -Path $cafile
-    Write-Output "Ecclesia Holding GmbH AD Sub1 CA" | Add-Content -Path $cafile
-    Write-Output "================================" | Add-Content -Path $cafile
-    Write-Output $sub1_ca | Add-Content -Path $cafile
+    Write-Output "Ecclesia Holding GmbH AD Root CA" | Add-Content -Path $caFile
+    Write-Output "================================" | Add-Content -Path $caFile
+    Write-Output $rootCa | Add-Content -Path $caFile
+    Write-Output "Ecclesia Holding GmbH AD Sub1 CA" | Add-Content -Path $caFile
+    Write-Output "================================" | Add-Content -Path $caFile
+    Write-Output $sub1Ca | Add-Content -Path $caFile
 }
 
 function ConfigureCurl() {
     if (Get-Command "curl" -ErrorAction SilentlyContinue) {
         Write-Output "Curl wird konfiguriert: CURL_CA_BUNDLE"
-        [Environment]::SetEnvironmentVariable("CURL_CA_BUNDLE", $cafile, "User")
+        [Environment]::SetEnvironmentVariable("CURL_CA_BUNDLE", $caFile, "User")
     }
 }
 
@@ -38,14 +38,14 @@ function ConfigureGit() {
     if (Get-Command "git" -ErrorAction SilentlyContinue) {
         Write-Output "Git wird konfiguriert: http.sslcainfo"
         & git config --global --unset http.sslverify
-        & git config --global http.sslcainfo "$cafile"
+        & git config --global http.sslcainfo "$caFile"
     }
 }
 
 function ConfigureNode() {
     if (Get-Command "node" -ErrorAction SilentlyContinue) {
         Write-Output "node.js wird konfiguriert: NODE_EXTRA_CA_CERTS"
-        [Environment]::SetEnvironmentVariable("NODE_EXTRA_CA_CERTS", $cafile, "User")
+        [Environment]::SetEnvironmentVariable("NODE_EXTRA_CA_CERTS", $caFile, "User")
     }
 }
 
@@ -59,71 +59,80 @@ function ConfigureDeno() {
 function ConfigureJava() {
     Write-Output "Java wird konfiguriert: cacerts"
 
-    $scoop_base_dir = "~/scoop/apps/"
+    $scoopBaseDir = "~/scoop/apps/"
     
-    if (Test-Path $scoop_base_dir) {
-        $scoop_javas = Get-ChildItem -Recurse -Path $scoop_base_dir -Depth 7 -Filter java.exe -File -Name | Select-String "current"
+    if (Test-Path $scoopBaseDir) {
+        $scoopJavas = Get-ChildItem -Recurse -Path $scoopBaseDir -Depth 7 -Filter java.exe -File -Name | Select-String "current"
 
-        for($i = 0; $i -le ($scoop_javas.count - 1); $i += 1) {
-            $java_location = (get-item ($scoop_base_dir + $scoop_javas[$i])).Directory.Parent.FullName
-            ConfigureJavaInstallation $java_location "Scoop"
+        for($i = 0; $i -le ($scoopJavas.count - 1); $i += 1) {
+            $javaLocation = (get-item ($scoopBaseDir + $scoopJavas[$i])).Directory.Parent.FullName
+            ConfigureJavaInstallation $javaLocation "Scoop"
         }
     }
     
     ConfigureJavaInstallation $env:JAVA_HOME "JAVA_HOME"
-}
-
-function ConfigureJavaInstallation($java_directory, $source) {
-    $java_binary = "$java_directory\bin\java.exe"
     
-    $root_ca_file = "$env:TEMP\root-ca.cer"
-    $sub1_ca_file = "$env:TEMP\sub1-ca.cer"
+    $temurinDir = $env:ProgramFiles + "\Eclipse Adoptium\"
 
-    if (Test-Path $java_binary) {
-        $root_ca | Out-File -Encoding ASCII -FilePath $root_ca_file
-        $sub1_ca | Out-File -Encoding ASCII -FilePath $sub1_ca_file
-
-        $java_version = (Get-Command $java_binary).Version.Major
-
-        Write-Output "- $java_directory (Quelle: $source; Version $java_version)"
-
-        ImportCert $java_directory $java_version $root_ca_file ecc_root-ca
-        ImportCert $java_directory $java_version $sub1_ca_file ecc_sub1-ca
-
-        Remove-Item $root_ca_file
-        Remove-Item $sub1_ca_file
+    $temurinJavas = Get-ChildItem -Recurse -Path $temurinDir -Depth 2 -Filter java.exe -File -Name
+    
+    for($i = 0; $i -le ($temurinJavas.count - 1); $i += 1) {
+        $javaLocation = (get-item ($temurinDir + $temurinJavas[$i])).Directory.Parent.FullName
+        ConfigureJavaInstallation $javaLocation "Eclipse Temurin"
     }
 }
 
-function ImportCert($java_directory, $java_version, $ca_file, $ca_alias) {
-    $keytool_binary = "$java_directory\bin\keytool.exe"
-    $cacerts_file = "$java_directory\jre\lib\security\cacerts"
+function ConfigureJavaInstallation($javaDirectory, $source) {
+    $javaBinary = "$javaDirectory\bin\java.exe"
     
-    if ($java_version -le 8) {
-        $present = & $keytool_binary -keystore "$cacerts_file" -storepass "changeit" -list | Select-String -Quiet -Pattern "$ca_alias"
+    $rootCaFile = "$env:TEMP\root-ca.cer"
+    $sub1CaFile = "$env:TEMP\sub1-ca.cer"
+
+    if (Test-Path $javaBinary) {
+        $rootCa | Out-File -Encoding ASCII -FilePath $rootCaFile
+        $sub1Ca | Out-File -Encoding ASCII -FilePath $sub1CaFile
+
+        $javaVersion = (Get-Command $javaBinary).Version.Major
+
+        Write-Output "- $javaDirectory (Quelle: $source; Version $javaVersion)"
+
+        ImportCert $javaDirectory $javaVersion $rootCaFile ecc_root-ca
+        ImportCert $javaDirectory $javaVersion $sub1CaFile ecc_sub1-ca
+
+        Remove-Item $rootCaFile
+        Remove-Item $sub1CaFile
+    }
+}
+
+function ImportCert($javaDirectory, $javaVersion, $caFile, $caAlias) {
+    $keytoolBinary = "$javaDirectory\bin\keytool.exe"
+    $cacertsFile = "$javaDirectory\jre\lib\security\cacerts"
+    
+    if ($javaVersion -le 8) {
+        $present = & $keytoolBinary -keystore "$cacertsFile" -storepass "changeit" -list | Select-String -Quiet -Pattern "$caAlias"
     } else {
-        $present = & $keytool_binary -cacerts -storepass "changeit" -list | Select-String -Quiet -Pattern "$ca_alias"
+        $present = & $keytoolBinary -cacerts -storepass "changeit" -list | Select-String -Quiet -Pattern "$caAlias"
     }
 
     if ($present) {
         return
     }
     
-    if ($java_version -le 8) {
-        & $keytool_binary -importcert -keystore "$cacerts_file" -storepass "changeit" -trustcacerts -noprompt -alias "$ca_alias" -file "$ca_file"
+    if ($javaVersion -le 8) {
+        & $keytoolBinary -importcert -keystore "$cacertsFile" -storepass "changeit" -trustcacerts -noprompt -alias "$caAlias" -file "$caFile"
     }  else {
-        & $keytool_binary -importcert -storepass "changeit" -cacerts -noprompt -alias "$ca_alias" -file "$ca_file"
+        & $keytoolBinary -importcert -storepass "changeit" -cacerts -noprompt -alias "$caAlias" -file "$caFile"
     } 
 }
 
 function ConfigureIntelliJIDEA() {
-    $jetbrains_root_dir = "$env:APPDATA\Jetbrains"
-    if (!(Test-Path $jetbrains_root_dir)) {
+    $jetbrainsRootDir = "$env:APPDATA\Jetbrains"
+    if (!(Test-Path $jetbrainsRootDir)) {
         return
     }
 
-    $intellij_dirs = Get-ChildItem $jetbrains_root_dir "IntellijIdea*" -Recurse -Directory
-    if ($intellij_dirs.count -eq 0) {
+    $intellijDirs = Get-ChildItem $jetbrainsRootDir "IntellijIdea*" -Recurse -Directory
+    if ($intellijDirs.count -eq 0) {
         return
     }
 
@@ -134,22 +143,22 @@ function ConfigureIntelliJIDEA() {
         return
     }
 
-    $src_cacerts_files = Get-ChildItem $env:JAVA_HOME "cacerts" -Recurse -File
-    if ($src_cacerts_files.count -ne 1) {
+    $srcCacertsFiles = Get-ChildItem $env:JAVA_HOME "cacerts" -Recurse -File
+    if ($srcCacertsFiles.count -ne 1) {
         Write-Output "! Es darf nur genau eine cacerts-Datei in der Java-Installation vorhanden sein"
         return
     }
-    $src_cacerts_file = $src_cacerts_files[0]
+    $srcCacertsFile = $srcCacertsFiles[0]
 
-    foreach ($intellij_dir in $intellij_dirs) {
-        $ssl_dir = "$intellij_dir\ssl"
-        $dst_cacert_file = "$ssl_dir\cacerts"
+    foreach ($intellijDir in $intellijDirs) {
+        $sslDir = "$intellijDir\ssl"
+        $dstCacertsFile = "$sslDir\cacerts"
         
-        Write-Output $ssl_dir
+        Write-Output $sslDir
 
-        if (Test-Path $ssl_dir) {
-            Write-Output "- $($intellij_dir.BaseName)"
-            Copy-Item -Path $src_cacerts_file.FullName -Destination $dst_cacert_file            
+        if (Test-Path $sslDir) {
+            Write-Output "- $($intellijDir.BaseName)"
+            Copy-Item -Path $srcCacertsFile.FullName -Destination $dstCacertsFile            
         }      
     }
 }
@@ -164,9 +173,9 @@ function ReadMachineCert ($certPath) {
     return $PemString
 }
 
-$cafile = "$env:USERPROFILE\ecclesia_cafile.pem"
+$caFile = "$env:USERPROFILE\ecclesia_cafile.pem"
 
-$root_ca = (ReadMachineCert 'Cert:\*56BD703E9E5A912012577B00DF6E313779EAC6AD')[-1]
-$sub1_ca = (ReadMachineCert 'Cert:\*8E997E9990e89EA4BB03C6379179A9FB4EC26D7F')[-1] 
+$rootCa = (ReadMachineCert 'Cert:\*56BD703E9E5A912012577B00DF6E313779EAC6AD')[-1]
+$sub1Ca = (ReadMachineCert 'Cert:\*8E997E9990e89EA4BB03C6379179A9FB4EC26D7F')[-1] 
 
 Main
